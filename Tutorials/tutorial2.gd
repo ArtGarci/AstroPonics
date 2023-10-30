9e02c65476945f9f6dd37fa9644f0cbce792a1ce
Hoodi3ac3 <aceron.vincent5@gmail.com>
2023-10-30 01:43:00 -0700

    Updated tutorial pages

--- Tutorials/tutorial2.gd
+++ Tutorials/tutorial2.gd
@@ -11,3 +11,7 @@ func _on_next_pressed():
 #prev button will change scene to page 1
 func _on_prev_pressed():
 	get_tree().change_scene_to_file("res://Tutorials/tutorial_page_1.tscn")
+
+#the exit button will go back to the main menu
+func _on_button_3_pressed():
+	get_tree().change_scene_to_file("res://main_menu_display.tscn")

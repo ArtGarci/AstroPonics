ddacc397077bc905122d67ea97698d291fabebf8
Finnick <flakeotes@gmail.com>
2023-10-31 21:50:16 -0700

    Menu Update 2
    Added more to menu and connected to other pages

--- START GAME.gd
+++ START GAME.gd
@@ -1,10 +1,10 @@
 extends Button
 
 func _on_pressed():
-	get_tree().change_scene_to_file("res://game.tscn")
+	get_tree().change_scene_to_file("res://Tutorials/tutorial_page_1.tscn")
 
 func _on_ref_guide_pressed():
-	get_tree().change_scene_to_file("res://reference_menu.tscn")
+	get_tree().change_scene_to_file("res://Objects/reference_menu.tscn")
 
 func _on_credits_info_pressed():
 	get_tree().change_scene_to_file("res://credits.tscn")

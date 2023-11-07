f20c542a9eed8b22e121f5ac8187a4424d898005
ArtGarci <143542702+ArtGarci@users.noreply.github.com>
2023-11-07 13:43:38 -0800

    Adding comments to code
    adding comments to code making it easier to understand what is going on

--- low_light_plant.gd
+++ low_light_plant.gd
@@ -4,21 +4,22 @@ extends Node2D
 var plantGrowing = false
 
 func _on_low_light_but_pressed():
-	if Global.itTrowel == true:
-		if plantGrowing == true:
-			plantGrowing = false
-			spriteAnim.animation = "none"
-			Global.seedSelected = 0
+	if Global.itTrowel == true:				# checks that trowel is picked
+		if plantGrowing == true:			# checks that a plant is growing
+			plantGrowing = false			# sets plant growing to false
+			spriteAnim.animation = "none"	# removes animation
+			Global.seedSelected = 0			# sets Global Variable seed selections to 0
 
 # 1=lettace 2=radish 3=chives 4=potatoes 5=wheat 6=tomatoes
 # when the seed goes over a grow plot, the plant animation starts
 func _on_low_plant_coll_area_entered(area):
-	if Global.itSeed == true:
-		if area.name == "seedArea":
-			if Global.lightLevel == 1:
+	if Global.itSeed == true:					# checks that seed is selected
+		if area.name == "seedArea":				# makes sure the sead is over pot area
+			if Global.lightLevel == 1:			# checks that seed light level is low
 				if Global.seedSelected == 0:
 					spriteAnim.play("none")
 					plantGrowing = false
+				# grows plant depending on global variable seedSelected and plant is not growing
 				if Global.seedSelected == 1 and plantGrowing == false:
 					spriteAnim.play("lettaceGrow")
 					plantGrowing = true
@@ -28,11 +29,6 @@ func _on_low_plant_coll_area_entered(area):
 				if Global.seedSelected == 3 and plantGrowing == false:
 					spriteAnim.play("chivesGrow")
 					plantGrowing = true
-	#if Global.itTrowel == true:
-	#	if plantGrowing == true:
-	#		plantGrowing = false
-	#		spriteAnim.animation = "none"
-	#		Global.seedSelected = 0
 
 # used for testing
 func setGlobalVariables(itSeed, lightLevel, seedSelected, itTrowel=false):
--- my_plant.gd
+++ my_plant.gd
@@ -4,22 +4,23 @@ extends Node2D
 var plantGrowing = false
 
 func _on_plant_button_pressed():
-	if Global.itTrowel == true:
-		if plantGrowing == true:
-			plantGrowing = false
-			spriteAnim.animation = "none"
-			Global.seedSelected = 0
+	if Global.itTrowel == true:				# checks that trowel is picked
+		if plantGrowing == true:			# checks that a plant is growing
+			plantGrowing = false			# sets plant growing to false
+			spriteAnim.animation = "none"	# removes animation
+			Global.seedSelected = 0			# sets Global Variable seed selections to 0
 
 # 1=lettace 2=radish 3=chives 4=potatoes 5=wheat 6=tomatoes
 # when the seed goes over a grow plot, the plant animation starts
 func _on_plant_collision_area_entered(area):
-	if Global.itSeed == true:
-		if area.name == "seedArea":
-			if Global.lightLevel == 2:
-				if Global.seedSelected == 0:
+	if Global.itSeed == true:					# checks that seed is selected
+		if area.name == "seedArea":				# makes sure the sead is over pot area
+			if Global.lightLevel == 2:			# checks that seed light level is high	
+				if Global.seedSelected == 0:	# default sets animation to none and plant false
 					spriteAnim.play("none")
 					plantGrowing = false
-				if Global.seedSelected == 4 and plantGrowing == false:
+				# grows plant depending on global variable seedSelected and plant is not growing
+				if Global.seedSelected == 4 and plantGrowing == false:	
 					spriteAnim.play("growPotatoes")	
 					plantGrowing = true
 				if Global.seedSelected == 5 and plantGrowing == false:
@@ -28,12 +29,6 @@ func _on_plant_collision_area_entered(area):
 				if Global.seedSelected == 6 and plantGrowing == false:
 					spriteAnim.play("growTomatoes")
 					plantGrowing = true
-	#if Global.itTrowel == true:
-	#	if plantGrowing == true:
-	#			plantGrowing = false
-	#			spriteAnim.animation = "none"
-	#			Global.seedSelected = 0
-	#			mouseClick = false
 
 # used for testing
 func setGlobalVariables(itSeed, lightLevel, seedSelected, isTrowel=false):

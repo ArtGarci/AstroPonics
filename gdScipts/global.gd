2cd5534c20bd5a18690b658fc6c241115d31abe9
ArtGarci <143542702+ArtGarci@users.noreply.github.com>
2023-11-16 12:09:20 -0800

    added test plants
    added tests for plants for the new animation which changes depending on "weeks" alive. also removed some uneeded global variables and commented my code

--- gdScipts/global.gd
+++ gdScipts/global.gd
@@ -2,8 +2,6 @@ extends Node
 
 var seedSelected = 0 # 1=lettace 2=radish 3=chives 4=potatoes 5=wheat 6=tomatoes
 var lightLevel = 0 #1=low,2=high
-var mouseClick = false # true seed following mouse
-var mouseEntered = false
 var itSeed = false
 var itTrowel = false
 #tmp for making plants work
--- gdScipts/trowel.gd
+++ gdScipts/trowel.gd
@@ -1,7 +1,5 @@
 extends Node2D
 
-var plantLoad = preload("res://scenes/my_plant.tscn")
-
 func _process(delta):
 	global_position = get_global_mouse_position()
 	
@@ -18,7 +16,6 @@ func _on_trowel_area_area_entered(area):
 			Global.seedSelected = 0
 			Global.lightLevel = 0
 			Global.itTrowel = false
-			Global.mouseClick = false
 			# gets trash bin node and hides it
 			var getArea = area.get_parent()
 			getArea = getArea.get_parent()

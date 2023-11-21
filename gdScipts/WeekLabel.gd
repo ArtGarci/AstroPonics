ba583ee37286e1b6faea222cbc5b03bf80b1e288
Vanessa <71130347+typewriterbot@users.noreply.github.com>
2023-11-21 01:17:22 -0800

    Implementation with Game scene
    Now Random Events works with current main game scene, the camera.gd are in this directory for organization, weeklabel.gd is changed to call random events (label).

--- gdScipts/WeekLabel.gd
+++ gdScipts/WeekLabel.gd
@@ -4,9 +4,13 @@ var weeks_passed = 0  # Initialize the number of weeks
 var elapsed_real_time = 0  # Initialize the elapsed real-time
 var seconds_per_gameplay_week = 10 * 60  # 10 minutes in seconds
 
+var t
 func _ready():
+	RE.build() 
+	t = RE.new() 
+	add_child(t)
 	update_weeks_display()
-	GlobalScoreLabel.set_score("Test", 100)
+	GlobalScoreLabel.set_score("TEST", 0)
 
 func _process(delta):
 	# Update elapsed real-time
@@ -17,7 +21,7 @@ func _process(delta):
 		weeks_passed += 1
 		elapsed_real_time -= seconds_per_gameplay_week
 		update_weeks_display()
-		
+		t.launcher(20) # ASSUMES that the node presides on same scene
 	#check if the game should end after 12 weeks
 	if weeks_passed >=12:
 		end_game() # call a function to end the game

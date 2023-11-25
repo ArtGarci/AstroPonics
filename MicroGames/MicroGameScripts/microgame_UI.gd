03c9c59494dfe6f5cb69c8fcfe325077e87fe85b
ChristopherMonteleone <christophergmonteleone@gmail.com>
2023-11-25 14:56:08 -0800

    pH Microgame Added
    pH Microgame and related files added

--- MicroGames/MicroGameScripts/microgame_UI.gd
+++ MicroGames/MicroGameScripts/microgame_UI.gd
@@ -4,7 +4,7 @@ func _on_water_micro_game_pressed():
 	get_tree().change_scene_to_file('res://MicroGames/MicroGameScenes/water_instructions.tscn')
 
 func _on_p_h_micro_game_pressed():
-	pass
+	get_tree().change_scene_to_file('res://MicroGames/MicroGameScenes/pH_instructions.tscn')
 
 func _on_trim_micro_game_pressed():
 	get_tree().change_scene_to_file('res://MicroGames/MicroGameScenes/trim_instructions.tscn')

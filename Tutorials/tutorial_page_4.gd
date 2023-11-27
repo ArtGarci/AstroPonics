e7d1c1dab9c60c646718eef95f7c781c50ae07e0
Hoodi3ac3 <aceron.vincent5@gmail.com>
2023-11-26 20:18:37 -0800

    Redesigned Random Events and Added more info in minigames

---
+++ Tutorials/tutorial_page_4.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+
+
+#next page
+func _on_texture_button_pressed():
+	get_tree().change_scene_to_file("res://Tutorials/tutorial_page_5.tscn")
+
+#prev page
+
+func _on_texture_button_2_pressed():
+	get_tree().change_scene_to_file("res://Tutorials/tutorial_page_3.tscn")
+	
+#main menu
+
+
+
+func _on_button_2_pressed():
+	get_tree().change_scene_to_file("res://main_menu_display.tscn")

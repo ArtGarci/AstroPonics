9c4dd865b12c10473de3e252c8c9a256e17fa4e8
Vanessa <71130347+typewriterbot@users.noreply.github.com>
2023-11-28 02:30:38 -0800

    Updated Random Events + PH effect
    the animate text script is no longer being used as was causing unintended side effects and not always showing so will find alternative way to emphasis event in action. Random event signal to PH global signal to affect plants + animation for user to know what is going on.

--- Setup_Alarm.gd
+++ Setup_Alarm.gd
@@ -1,9 +1,28 @@
 extends Sprite2D
 
+var tween 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
+
+func animate() -> void:
+	if (tween): # tweens are not designed to be re-used 
+		tween.kill() # abort 
+	tween = get_tree().create_tween().set_parallel(true)
+	tween.tween_property(self, "position", self.position, 1)
+	tween.tween_property(self, "scale", Vector2(1,1), 1).set_ease(Tween.EASE_OUT)
+	tween.tween_property(self, "rotation_degrees", 360, 1).as_relative()
+	tween.tween_property(self, "scale", Vector2(3,3), 1).set_ease(Tween.EASE_OUT)
+	tween.play()
+	
+func animate_away():
+	if (tween): # tweens are not designed to be re-used 
+		tween.kill() # abort 
+	tween = get_tree().create_tween() # binds object w/ tween 
+	tween.bind_node(self) # .set_trans(Tween.TRANS_ELASTIC)
+	tween.tween_property(self, "modulate", Color.RED, 1).set_trans(Tween.TRANS_SINE)
+	tween.tween_property(self, "scale", Vector2(), 1).set_trans(Tween.TRANS_BOUNCE)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.

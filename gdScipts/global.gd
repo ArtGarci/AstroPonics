08e9471f9eeadd7d20f4662ec9e6516cdfc21ce7
MayDim <maylynn.dimalanta@gmail.com>
2023-11-30 10:40:08 -0800

    Added a NextWeek button
    Added a next week button. Should also check for random events as well.

--- gdScipts/global.gd
+++ gdScipts/global.gd
@@ -4,6 +4,7 @@ var seedSelected = 0 # 1=lettace 2=radish 3=chives 4=potatoes 5=wheat 6=tomatoes
 var lightLevel = 0 #1=low,2=high
 var itSeed = false
 var itTrowel = false
+var microGameScore = 0
 #tmp for making plants work
 signal nextWeekSignal
 signal PHEFFECT # why not ;p;

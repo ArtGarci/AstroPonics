e50fbdc8628e3a160f45f89255b597b713e66673
ArtGarci <143542702+ArtGarci@users.noreply.github.com>
2023-12-01 01:03:24 -0800

    added bool to signal
    added bool to signal that will change ph 100% of time. commented out push warning due to excessive warnings in debugger.

--- gdScipts/my_plant.gd
+++ gdScipts/my_plant.gd
@@ -12,9 +12,9 @@ func _ready():
 	Global.connect("PHEFFECT", Callable(self, "phChange"))
 	#signal from mini game that fixes ph here
 
-func phChange():
+func phChange(reBool=false):
 	# random number generated and if 0 or 1 then PH is changed (20% chance)
-	if (randi() % 10) < 2:
+	if (randi() % 10) < 2 or reBool == true:
 		# random generated and if 0 then add random number between 1 and 3
 		if randi() % 2 == 0:
 			currPH = currPH + randf_range(1,3)	# adds random number between 1 and 3
